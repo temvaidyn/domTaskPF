2959a2a08480d2388138030345f523120704dede
temvaidyn <temirkhanovaidyn@gmail.com>
2022-05-17 19:51:07 +0100

    Class Work

--- main.js
+++ main.js
@@ -5,8 +5,25 @@ function roundUp(x) {
 
 function CalculateMagic(a, b) {
     // add your code here
-    
-    
+    let number = a + b + b;
+    number += 90;
+    number = number + 90;
+    number = number / 10;
+
+    if (number % 5 == 0 ){
+        number = number - 8;
+    }
+    else{
+        number - number + 9;
+    }
+
+    number = number * number; 
+    // or number = number ** 2;
+
+    let G = number / 3;
+
+    return roundUp(G);
+
     // remember to call roundUp() with your result before returning
     // return roundUp(g);
 }
